91dada61e65e8d76fcbf48a87d8d00eef808f791
Miguel Teixeira <miguel.teixeira@paddypowerbetfair.com>
2022-03-31 09:33:10 +0100

    Rename to ingestor

--- fizz-ingestor/src/main/java/pt/teixeiram2/tracing/FizzIngestorApplication.java
+++ fizz-ingestor/src/main/java/pt/teixeiram2/tracing/FizzIngestorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FizzGeneratorApplication {
+public class FizzIngestorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FizzGeneratorApplication.class, args);
+		SpringApplication.run(FizzIngestorApplication.class, args);
 	}
 
 }
